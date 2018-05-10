0fbf0f16a2fd48c2d2d8351a76f9927956c2f1c3
barry <zuohengsong@1dabang.cn>
2018-05-10 11:22:54 +0800

    创建项目

---
+++ CreateComponentDemo/CreateComponentDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CreateComponentDemo
+//
+//  Created by Barry on 2018/5/10.
+//  Copyright © 2018年 Barry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CreateComponentDemo/CreateComponentDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CreateComponentDemo
+//
+//  Created by Barry on 2018/5/10.
+//  Copyright © 2018年 Barry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
